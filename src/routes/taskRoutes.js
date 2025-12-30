221e4b5d2c69e262db51ca90dda6c313c5fbd049
Pdash242 <124784374+Pdash242@users.noreply.github.com>
2025-12-30 12:05:19 +0530

    Create taskRoutes.js

---
+++ src/routes/taskRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const auth = require('../middlewares/authMiddleware');
+const { createTask, getTasks, updateTask, deleteTask } = require('../controllers/taskController');
+
+const router = express.Router();
+
+router.post('/', auth, createTask);
+router.get('/', auth, getTasks);
+router.put('/:id', auth, updateTask);
+router.delete('/:id', auth, deleteTask);
+
+module.exports = router;
